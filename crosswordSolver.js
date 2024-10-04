52919d42eb50231b4aa63fcbe7991c699ee0f3f4
amine habchi <104910967+aminehabchi@users.noreply.github.com>
2024-10-05 00:19:37 +0100

    Add files via upload

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,98 +1,133 @@
-const puzzle = `2001
-0..0
-1000
-0..0`
-const words = ['casa', 'alan', 'ciao', 'anta']
+const puzzle = `...1...........
+..1000001000...
+...0....0......
+.1......0...1..
+.0....100000000
+100000..0...0..
+.0.....1001000.
+.0.1....0.0....
+.10000000.0....
+.0.0......0....
+.0.0.....100...
+...0......0....
+..........0....`
+const words = [
+  'sun',
+  'sunglasses',
+  'suncream',
+  'swimming',
+  'bikini',
+  'beach',
+  'icecream',
+  'tan',
+  'deckchair',
+  'sand',
+  'seaside',
+  'sandals',
+].reverse()
+
 crosswordSolver(puzzle, words)
+
 function crosswordSolver(ep, words) {
-    let emptyPuzzle = convertTo2DArray(ep.split("\n"))
-    let wordMap=convertWordToMap(words)
-    console.log(backtrack(emptyPuzzle,wordMap))
-    console.log(getWords(wordMap,0,4))
+    let emptyPuzzle = ep.split("\n")
+    let epp = []
+    for (let i = 0; i < emptyPuzzle.length; i++) {
+        epp.push(stringToBytes(emptyPuzzle[i]))
+    }
+    let w = []
+    for (let i = 0; i < words.length; i++) {
+        w.push(stringToBytes(words[i]))
+    }
+   //console.log(writeVertical(epp, 0, 0, w[0]))
+    let o=backtrack(epp,epp, w)
+    if (o=='Error'){
+        console.log(o)
+    }else{
+        let a=byteArray2DToStringArray(o);
+        for (let i=0;i<a.length;i++){
+            console.log(a[i])
+        }
+    }
+}
+function byteArray2DToStringArray(byteArray2D) {
+    return byteArray2D.map(byteArray => {
+        return String.fromCharCode(...byteArray);
+    });
+}
+function stringToBytes(str) {
+    const encoder = new TextEncoder();
+    const byteArray = encoder.encode(str);
+    return Array.from(byteArray);
 }
 
-function backtrack(emptyPuzzle, wordmap) {
-    for (let i = 0; i < emptyPuzzle.length; i++) {
-        for (let j = 0; j < emptyPuzzle[i].length; j++) {
-            if (emptyPuzzle[i][j]>'0' && emptyPuzzle[i][j]<='9'){
-                let right=clRight(emptyPuzzle,i,j)
-                let Left=clLeft(emptyPuzzle,i,j)
-                
+
+function backtrack(Puzzle, emptyPuzzle, words) {
+    if (words.length === 0) {
+        return emptyPuzzle;  // Return the completed puzzle instead of 'Error'
+    }
+    // Iterate over the Puzzle grid
+    for (let i = 0; i < Puzzle.length; i++) {
+        for (let j = 0; j < Puzzle[i].length; j++) {
+            if (Puzzle[i][j] == 49 || Puzzle[i][j] == 50) {
+
+                // Deep copy the emptyPuzzle for vertical/horizontal placements
+                let coppyVertical = emptyPuzzle.map(arr => arr.slice());
+                let coppyHorizontal = emptyPuzzle.map(arr => arr.slice());
+
+                // Try placing the word vertically
+                if (writeVertical(coppyVertical, i, j, words[0]) !== 'Error') {
+                    let puzzleCopy = Puzzle.map(arr => arr.slice());  // Deep copy Puzzle
+                    puzzleCopy[i][j]--;  // Decrease constraint
+
+                    // Recurse with the updated puzzle and the remaining words
+                    const result = backtrack(puzzleCopy, coppyVertical, words.slice(1));
+                    if (result !== 'Error') {
+                        return result;
+                    }
+                }
+
+                // Try placing the word horizontally
+                if (writeHorizantale(coppyHorizontal, i, j, words[0]) !== 'Error') {
+                    let puzzleCopy = Puzzle.map(arr => arr.slice());  // Deep copy Puzzle
+                    puzzleCopy[i][j]--;  // Decrease constraint
+
+                    // Recurse with the updated puzzle and the remaining words
+                    const result = backtrack(puzzleCopy, coppyHorizontal, words.slice(1));
+                    if (result !== 'Error') {
+                        return result;
+                    }
+                }
             }
         }
     }
 
-    return emptyPuzzle
+    return 'Error';  // Return 'Error' if no solution is found
 }
 
-function getWords(wordmap,l,r){
-    let w=[]
-    for (let [_, value] of wordmap) {
-        for (let j=0;j<value.length;j++){
-            if (value[j].length==l || value[j].length==r){
-                w.push(value[j])
-            }   
-        }
-    }
-    return w
-}
 
-function convertTo2DArray(arr) {
-    return arr.map(str => str.split(''));
-}
 function writeHorizantale(e, x, y, w) {
+    if (y + w.length > e[x].length) {
+        return 'Error'
+    }
     for (let i = y; i < y + w.length; i++) {
-        e[x][i] = w[i - y]
+        if (e[x][i] == 50 || e[x][i] == 48 || e[x][i] == 49) {
+            e[x][i] = w[i - y]
+        } else if (e[x][i] != w[i - y]) {
+            return 'Error'
+        }
     }
     return e
 }
 function writeVertical(e, x, y, w) {
+    if (x + w.length > e.length) {
+        return 'Error'
+    }
     for (let i = x; i < x + w.length; i++) {
-        if (e[i][y]>='a' && e[i][y]>='Z'){
-
-        }else{
+        if (e[i][y] == 50 || e[i][y] == 48 || e[i][y] == 49) {
             e[i][y] = w[i - x]
+        } else if (e[i][y] != w[i - x]) {
+            return 'Error'
         }
     }
     return e
-}
-// console.log(convertWordToMap(words))
-
-function convertWordToMap(word) {
-    const wordMap = new Map()
-    for (let i = 0; i < word.length; i++) {
-        if (wordMap.has(word[i][0])) {
-            let a = wordMap.get(word[i][0])
-            a.push(word[i])
-            wordMap.set(word[i][0], a)
-        } else {
-            wordMap.set(word[i][0], [word[i]])
-        }
-    }
-    return wordMap
-}
-
-
-function clRight(e, x, y) {
-    let a = 0
-    for (let j = y; j < e[x].length; j++) {
-        if (e[x][j] != '.') {
-            a = a + 1
-        } else {
-            break
-        }
-    }
-    return a
-}
-
-function clLeft(e, x, y) {
-    let a = 0
-    for (let j = x; j < e.length; j++) {
-        if (e[j][y] != '.') {
-            a = a + 1
-        } else {
-            break
-        }
-    }
-    return a
 }
--- test.js
+++ test.js
@@ -2,4 +2,4 @@ const originalArray = [1, 2, 3, 4, 5];
 const copiedArray = originalArray.slice();
 copiedArray[0]=100
 console.log(originalArray)
-console.log(copiedArray)
+console.log(copiedArray.slice(2))
